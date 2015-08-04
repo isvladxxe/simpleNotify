973c66677a79fb966cce092a167e1eab1438c52d
isvladxxe <isvlad404@gmail.com>
2015-08-05 01:37:22 +0300

    create

---
+++ simpleNotify/Notify.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
+using System.Windows.Forms;
+namespace simpleNotify
+{
+    public static class Notify
+    {
+        public static void Activate(ToastNotification toastNotification)
+        {
+            ToastNotificationManager.CreateToastNotifier(Application.ProductName).Show(toastNotification);
+        }
+
+
+        static public ToastNotification Create(string title, string text, string image)
+        {
+            XmlDocument toastXml;
+            if (image == "")
+            {
+                toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+            }
+            else
+            {
+                toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText02);
+                XmlNodeList imageElements = toastXml.GetElementsByTagName("image");
+                imageElements[0].Attributes.GetNamedItem("src").NodeValue = "file:///" + Path.GetFullPath(image);
+            }
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+
+            stringElements[0].AppendChild(toastXml.CreateTextNode(title));
+            stringElements[1].AppendChild(toastXml.CreateTextNode(text));
+            return new ToastNotification(toastXml);
+        }
+        static public ToastNotification Create(string title, string text)
+        {
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+            stringElements[0].AppendChild(toastXml.CreateTextNode(title));
+            stringElements[1].AppendChild(toastXml.CreateTextNode(text));
+            return new ToastNotification(toastXml);
+        }
+        static public ToastNotification Create(string titleOrText)
+        {
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+            stringElements[0].AppendChild(toastXml.CreateTextNode(titleOrText));
+            return new ToastNotification(toastXml);
+        }
+        static public void Show(string title, string text, string image)
+        {
+            Activate(Create(title, text, image));
+        }
+        static public void Show(string title, string text)
+        {
+            Activate(Create(title, text));
+        }
+        static public void Show(string titleOrText)
+        {
+            Activate(Create(titleOrText));
+        }
+
+    }
+}
---
+++ simpleNotify/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("simpleNotify")]
+[assembly: AssemblyDescription("Easy notifications on win 8 or more")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("XXE")]
+[assembly: AssemblyProduct("simpleNotify")]
+[assembly: AssemblyCopyright("Isambaev Vladislav vk.com/isvlad")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e6f74be8-5adc-43a2-9612-de402ad41358")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
